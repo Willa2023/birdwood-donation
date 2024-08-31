9a44276f7f06076718a83bb2db37333913149b66
Willa2023 <142073064+Willa2023@users.noreply.github.com>
2024-08-31 18:12:58 +1200

    update donate form with mui

--- src/Components/Donate.js
+++ src/Components/Donate.js
@@ -2,6 +2,10 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import DonateForm from "./DonateForm";
 
 const Donate = () => {
+  const handleDonateClick = ()=>{
+    window.location.href='https://buy.stripe.com/test_aEU7t87Eb6G938kfYY';
+  }
+
   
   return (
     <section className="donate" id="donate">
@@ -15,7 +19,7 @@ const Donate = () => {
               <p><strong>To help, contact:</strong></p>
               <p>Dawn at dawn@sgcnz.org.nz</p>
               <p>Siva at karorikaitiaki@gmail.com</p>
-              <Button variant="primary">Donate</Button>
+              <Button variant="primary" onClick={handleDonateClick}>Donate</Button>
             </Col>
             <DonateForm />
         </Row>
--- src/Components/DonateForm.js
+++ src/Components/DonateForm.js
@@ -1,49 +1,249 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
-import { handleFormSubmit } from "../utils/formUtils";
+import { handleFormSubmit } from '../utils/formUtils';
+import { TextField, MenuItem } from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
+const currencies = [
+  {
+    value: 'Donated',
+    label: 'Donate Today',
+  },
+  {
+    value: 'Pledged',
+    label: 'Donate Later',
+  },
+];
 const DonateForm = () => {
-    const formInitialDetails = {
-        name: '',
-        address: '',
-        postcode: '',
-        email: '',
-        donatedAmount: '',
-        donatedDate: '',
-        pledgedAmount: '',
-        pledgedDate: '',
+  const [donateTime, setDonateTime] = useState('Donated');
+
+  const formInitialDetails = {
+    name: '',
+    address: '',
+    postcode: '',
+    email: '',
+    donatedAmount: '',
+    donatedDate: '',
+    pledgedAmount: '',
+    pledgedDate: '',
+  };
+
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Submit');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (key, value) => {
+    setFormDetails({
+      ...formDetails,
+      [key]: value,
+    });
+  };
+
+  const handleAmountChange = (key, value) => {
+    const inputAmount = parseInt(value, 10);
+    if (inputAmount < 1 || isNaN(inputAmount)) {
+      alert('Please enter a valid amount greater than 0.');
+      onFormUpdate(key, 1);
+    } else {
+      onFormUpdate(key, inputAmount);
     }
-    
-    const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const [buttonText, setButtonText] = useState('Submit');
-    const [status, setStatus] = useState({});
-    
-    const onFormUpdate = (key, value) => {
-        setFormDetails({
-            ...formDetails,
-            [key]: value,
-        });
-    }
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        handleFormSubmit(
-            formDetails,
-            'donateform',
-            setButtonText,
-            setStatus,
-            setFormDetails,
-            formInitialDetails
-        );
-        setFormDetails(formInitialDetails);
-    }
-    
-    return (
-        <>
-            <Col md={6} className="donate-form">
-                <h2>Donation Form</h2>
-                <form onSubmit={handleSubmit}>
-                    <Row>
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    handleFormSubmit(
+      formDetails,
+      'donateform',
+      setButtonText,
+      setStatus,
+      setFormDetails,
+      formInitialDetails
+    );
+    setFormDetails(formInitialDetails);
+  };
+
+  return (
+    <>
+      <Col md={6} className="donate-form">
+        <h2>Donation Form</h2>
+        <form onSubmit={handleSubmit}>
+          <Row>
+            <Col sm={12} className="px-1">
+              <TextField
+                required
+                fullWidth
+                id="outlined-required"
+                label="Name of Donor"
+                sx={{
+                  mb: 1,
+                  backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                  borderRadius: '4px',
+                }}
+                onChange={(e) => onFormUpdate('name', e.target.value)}
+              />
+            </Col>
+            <Col sm={12} className="px-1">
+              <TextField
+                fullWidth
+                id="outlined-multiline-static"
+                label="Address"
+                multiline
+                rows={2}
+                sx={{
+                  mb: 1,
+                  backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                  borderRadius: '4px',
+                }}
+                onChange={(e) => onFormUpdate('address', e.target.value)}
+              />
+            </Col>
+            <Col sm={12} className="px-1">
+              <TextField
+                fullWidth
+                id="outlined-required"
+                label="Post Code"
+                sx={{
+                  mb: 1,
+                  backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                  borderRadius: '4px',
+                }}
+                onChange={(e) => onFormUpdate('postcode', e.target.value)}
+              />
+            </Col>
+            <Col sm={12} className="px-1">
+              <TextField
+                fullWidth
+                required
+                id="outlined-required"
+                label="Email"
+                sx={{
+                  mb: 1,
+                  backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                  borderRadius: '4px',
+                }}
+                onChange={(e) => onFormUpdate('email', e.target.value)}
+              />
+            </Col>
+            <Col sm={6} className="px-1">
+              <TextField
+                fullWidth
+                id="outlined-select-currency"
+                select
+                label="Select when you donate"
+                value={donateTime}
+                onChange={(e) => setDonateTime(e.target.value)}
+                sx={{
+                  mb: 1,
+                  backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                  borderRadius: '4px',
+                }}
+              >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Col>
+
+            {donateTime === 'Donated' && (
+              <>
+                <Col sm={6} className="px-1">
+                  <TextField
+                    id="outlined-required"
+                    label="Donated(NZD)"
+                    fullWidth
+                    required
+                    type="number"
+                    inputProps={{ min: 1 }}
+                    sx={{
+                      backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                      borderRadius: '4px',
+                    }}
+                    onChange={(e) =>
+                      handleAmountChange('donatedAmount', e.target.value)
+                    }
+                  />
+                </Col>
+                <Col sm={12} className="px-1">
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['DatePicker']}>
+                      <DatePicker
+                        label="Date paid into RC Karori Trust Account"
+                        sx={{
+                          mb: 1,
+                          width: '100%',
+                          backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                          borderRadius: '4px',
+                        }}
+                        onChange={(date) => onFormUpdate('donatedDate', date)}
+                      />
+                    </DemoContainer>
+                  </LocalizationProvider>
+                </Col>
+              </>
+            )}
+
+            {donateTime === 'Pledged' && (
+              <>
+                <Col sm={6} className="px-1">
+                  <TextField
+                    id="outlined-required"
+                    label="Pledged(NZD)"
+                    fullWidth
+                    required
+                    type="number"
+                    inputProps={{ min: 1 }}
+                    sx={{
+                      backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                      borderRadius: '4px',
+                    }}
+                    onChange={(e) =>
+                      handleAmountChange('pledgedAmount', e.target.value)
+                    }
+                  />
+                </Col>
+                <></>
+                <Col sm={12} className="px-1">
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer fullWidth components={['DatePicker']}>
+                      <DatePicker
+                        label="Amount will be paid before: "
+                        sx={{
+                          mb: 1,
+                          width: '100%',
+                          backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                          borderRadius: '4px',
+                        }}
+                        onChange={(date) => onFormUpdate('pledgedDate', date)}
+                      />
+                    </DemoContainer>
+                  </LocalizationProvider>
+                </Col>
+              </>
+            )}
+            <Col sm={12} className="px-1">
+              <Button
+                variant="primary"
+                type="submit"
+                className="submit-donate-form"
+              >
+                <span>{buttonText}</span>
+              </Button>
+            </Col>
+            {status.message && (
+              <Col>
+                <p className={status.success === false ? 'danger' : 'success'}>
+                  {status.message}
+                </p>
+              </Col>
+            )}
+          </Row>
+
+          {/* <Row>
                         <Col sm={12}  className='px-1'>
                             <input type="text" value={formDetails.name} placeholder="Name" onChange={(e)=> onFormUpdate('name', e.target.value)} />
                         </Col>
@@ -54,16 +254,16 @@ const DonateForm = () => {
                             <input type="number" value={formDetails.postcode} placeholder="Post Code" onChange={(e)=> onFormUpdate('postcode', e.target.value)} />
                         </Col>
                         <Col sm={12} className='px-1'>
-                            <input type="email" value={formDetails.email} placeholder="Email"  onChange={(e)=> onFormUpdate('email', e.target.value)} />
+                            <input required type="email" value={formDetails.email} placeholder="Email"  onChange={(e)=> onFormUpdate('email', e.target.value)} />
                         </Col>
                         <Col sm={6} className='px-1'>
-                          <input type="number" value={formDetails.donatedAmount} placeholder="Donated Amount"  onChange={(e)=> onFormUpdate('donatedAmount', e.target.value)} />
+                          <input type="number" value={formDetails.donatedAmount} placeholder="Donated Amount" min="1" onChange={(e)=> handleAmountChange('donatedAmount', e.target.value)} />
                         </Col>
                         <Col sm={6} className='px-1'>
                             <input type="date" value={formDetails.donatedDate} placeholder="Donated Date"  onChange={(e)=> onFormUpdate('donatedDate', e.target.value)} />
                         </Col>
                         <Col sm={6} className='px-1'>
-                            <input type="number" value={formDetails.pledgedAmount} placeholder="Pledged Amount"  onChange={(e)=> onFormUpdate('pledgedAmount', e.target.value)} />
+                            <input type="number" value={formDetails.pledgedAmount} placeholder="Pledged Amount" min="1" onChange={(e)=> handleAmountChange('pledgedAmount', e.target.value)} />
                         </Col>
                         <Col sm={6} className='px-1'>
                             <input type="date" value={formDetails.pledgedDate} placeholder="Pledged Date"  onChange={(e)=> onFormUpdate('pledgedDate', e.target.value)} />
@@ -77,11 +277,12 @@ const DonateForm = () => {
                                 <p className={status.success === false ? 'danger' : 'success'}>{status.message}</p>
                             </Col>
                         }
-                    </Row>
-                </form>
-            </Col>
-        </>
-    )
- }
+                    </Row> */}
+        </form>
+        {/* //You agree to receive email communication from us by submitting this form and understand that your contact information will be stored with us. */}
+      </Col>
+    </>
+  );
+};
 
- export default DonateForm;
+export default DonateForm;
